4aa65a7edc247b8112afeb3785fd1850163fdcbe
Kristof Raes <kristof.raes@arteveldehs.be>
2022-10-20 11:42:42 +0200

    Add files via upload

---
+++ Opdracht 02/Program.cs
@@ -0,0 +1,11 @@
+﻿internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        // Willekeurig getal
+        // Willekeurig getal met bereik (van - tot)
+        // Willekeurige getallen Lotto...
+        // Trekking van de Lotto...
+    }
+}
